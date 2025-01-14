444e917d7b919e886e0e6d2ff67f5b989eb9174d
SaLaam17 <slaamri@yahoo.fr>
2025-01-14 10:51:39 +0100

    print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Vérifier s'il y a des arguments de ligne de commande
-if len(sys.argv) > 1:
-    # Boucler à travers tous les arguments de la ligne de commande
-    for i in range(len(sys.argv)):
-        print(f"Argument {i}: {sys.argv[i]}")
-else:
-    print("Aucun argument de ligne de commande fourni.")
+for i in range(1, len(sys.argv)):  # Commencer à partir de 1 pour ignorer le nom du fichier
+    print(sys.argv[i])
